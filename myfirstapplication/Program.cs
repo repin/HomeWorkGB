f3d628e81fce1d644a66098a52525491975adaef
repin <repin1990@gmail.com>
2021-12-15 22:30:26 +0300

    Добавьте файлы проекта.

---
+++ myfirstapplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myfirstapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Введите имя пользователя:");
+            string userNAme = System.Console.ReadLine();
+            System.Console.WriteLine($"Привет, {userNAme}, сегодня {System.DateTime.Now}!");
+            System.Console.ReadLine();
+        }
+    }
+}
