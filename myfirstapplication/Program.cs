769a425f5b726c516442502efff53eaeeb954b50
repin <sasharepin@Mail.ru>
2021-12-21 08:18:58 +0300

    Finish lesson-2-1

--- myfirstapplication/Program.cs
+++ myfirstapplication/Program.cs
@@ -6,9 +6,13 @@ namespace myfirstapplication
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Введите имя пользователя:");
-            string userNAme = System.Console.ReadLine();
-            System.Console.WriteLine($"Привет, {userNAme}, сегодня {System.DateTime.Now}!");
+            System.Console.WriteLine("Дробные числа необходимо вводить с разделителем системы, по умолчанию ','");
+            System.Console.WriteLine("Введите минимальную температуру за сутки:");
+            string tempMin = System.Console.ReadLine();
+            System.Console.WriteLine("Введите максимальную температуру за сутки:");
+            string tempMax = System.Console.ReadLine();
+            double tempMid = (Convert.ToDouble(tempMin) +Convert.ToDouble(tempMax))/2;
+            System.Console.WriteLine($"Средняя температура за сутки {tempMid.ToString()} градуса");
             System.Console.ReadLine();
         }
     }
