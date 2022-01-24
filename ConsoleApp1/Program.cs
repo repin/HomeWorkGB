b4839e6a0561abc85afc0429e3ede0e7c2c2aa29
repin <sasharepin@Mail.ru>
2022-01-24 19:49:50 +0300

    final lesson-8

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestStartField();
+            string t = Properties.Settings.Default.SayStart;
+            Console.WriteLine(t);
+            string name = InputData("Введите своё имя:");
+            string year = InputData("Введите свой возраст:");
+            string work = InputData("Введите свой род деятельности:");
+            Properties.Settings.Default.Name = name;
+            Properties.Settings.Default.Year = year;
+            Properties.Settings.Default.Work = work;
+            Properties.Settings.Default.Save();
+
+        }
+
+        static string InputData (string txt)
+        {
+            Console.WriteLine(txt);
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                return "no data";
+            }
+            return input;
+        }
+        static void TestStartField()
+        {
+            Console.WriteLine("Данные введенные в прошлый запуск:");
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.Name))
+            {
+                Console.WriteLine($"Ваше имя: {Properties.Settings.Default.Name}");
+            }
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.Year))
+            {
+                Console.WriteLine($"Ваш возраст: {Properties.Settings.Default.Year}");
+            }
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.Work))
+            {
+                Console.WriteLine($"Ваше род деятельности: {Properties.Settings.Default.Work}");
+            }
+        }
+    }
+}
